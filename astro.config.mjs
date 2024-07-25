4e2221de7777597e8beadebb0489b049e25af360
Sebas5158R <sebastianriveraa4@gmail.com>
2024-07-24 19:33:32 -0500

    feat: Add base URL for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // Para que funcione en GitHub Pages
+    site: 'https://Sebas5158R.github.io',
+    base: 'solar-system-astro',
+});
